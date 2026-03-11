c8b79e3a56794e46b830e2039f404ac7fe4fffd9
Ruben Carrascal <krrskl97@gmail.com>
2026-03-11 14:42:02 -0500

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# amaris_test
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/dev/codingvault/amaris_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.codingvault.amaris_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
